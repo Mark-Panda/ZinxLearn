a4f62b66ce99ffdf3fc91147c0fad3183064950e
Simple_Panda <simple_95@163.com>
2019-11-22 14:21:26 +0800

    第一次

---
+++ go.mod
@@ -0,0 +1,3 @@
+module zinxText
+
+go 1.13
---
+++ znet/server.go
@@ -0,0 +1,96 @@
+package znet
+
+import (
+	"fmt"
+	"net"
+	"zinxText/ziface"
+)
+
+//IServer的接口实现，定义一个个Server的服务器模块
+
+type Server struct {
+	//服务器名称
+	Name string
+	//服务器绑定的IP版本
+	IPVersion string
+	//服务器监听的IP
+	IP string
+	//服务器监听的端口
+	Port string
+}
+//启动服务器
+func (s *Server) Start()  {
+	fmt.Println("[Start] Server Listenner at IP:%s, Port %d, is starting\n ", s.IP, s.Port)
+	go func() {
+		// 1.获取一个TCP的Address
+		addr, err := net.ResolveTCPAddr(s.IPVersion, fmt.Sprintf("%s:%d", s.IP,s.Port))
+		if err != nil {
+			fmt.Println("resolve tcp aaddt err:", err)
+			return
+		}
+		//2.监听服务器地址
+		listenner, err := net.ListenTCP(s.IPVersion, addr)
+		if err != nil {
+			fmt.Println("listent", s.IPVersion, "err", err)
+			return
+		}
+
+		fmt.Println("start Zinx server succ", s.Name, "succ, Listening...")
+		//3.阻塞的等待客户端连接，处理客户端连接业务
+		for {
+			//如果有客户端连接过来，阻塞会返回
+			conn, err := listenner.AcceptTCP()
+			if err != nil {
+				fmt.Println("Accept err", err)
+				continue
+			}
+
+			//已经与客户端建立连接，做一些业务，做一个最基本的最大512字节长度的回显业务
+			go func() {
+				for {
+					buf := make([]byte, 512)
+					cnt, err := conn.Read(buf)
+					if err != nil {
+						fmt.Println("recv buf err", err)
+						continue
+					}
+					fmt.Printf("recv client buf %s, cnt %d\n", buf, cnt)
+					//回显功能
+					if _, err := conn.Write(buf[:cnt]); err != nil {
+						fmt.Println("write back buf err", err)
+						continue
+					}
+				}
+			}()
+		}
+	}()
+}
+//停止服务器
+func (s *Server) Stop()  {
+	//TODO 将一些服务器的资源，状态或者一些已经开辟的链接信息，进行停止或修改
+}
+//运行服务器
+func (s *Server) Serve()  {
+	//启动server的服务功能
+	s.Start()
+
+	//TODO 做一些启动服务器之后的额外业务
+
+	//阻塞状态
+	select {
+
+	}
+}
+
+/**
+初始化Server模块的方法
+ */
+func NewServer(name string) ziface.IServer {
+	s := &Server{
+		Name:      name,
+		IPVersion: "tcp4",
+		IP:        "0.0.0.0",
+		Port:      "8999",
+	}
+	return s
+}
